f7c6db19765698313f1705d52c03e662b66f0caf
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-03-07 20:15:00 -0800

    proxy server MVP

--- server/routers/data.js
+++ server/routers/data.js
@@ -2,12 +2,22 @@ var express = require('express');
 var router = express.Router();
 //this router deals with /api/restaurants
 
-router.get('/:id/sidebar', (req, res) => {
-  res.redirect('http://localhost:3003/api/restaurants/' + req.params.id + '/sidebar');
+router.get('/:id/gallery', (req, res) => {
+  res.redirect('http://localhost:3001/api/restaurants/' + req.params.id + '/gallery');
 });
 
 router.get('/:id/overview', (req, res) => {
   res.redirect('http://localhost:3002/api/restaurants/' + req.params.id + '/overview');
 });
+
+router.get('/:id/sidebar', (req, res) => {
+  res.redirect('http://localhost:3003/api/restaurants/' + req.params.id + '/sidebar');
+});
+
+router.get('/:id/recommendations', (req, res) => {
+  res.redirect('http://localhost:3004/api/restaurants/' + req.params.id + '/recommendations');
+});
+
+
 
 module.exports = router;
