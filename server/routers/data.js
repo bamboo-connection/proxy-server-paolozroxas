f7e18c39adf97df1102ab6bd63a4c64ce0b5e184
Paolo Z Roxas <paolo.z.roxas@gmail.com>
2018-04-19 18:44:27 -0700

    changed route addresses

--- server/routers/data.js
+++ server/routers/data.js
@@ -3,19 +3,19 @@ var router = express.Router();
 //this router deals with /api/restaurants
 
 router.get('/:id/gallery', (req, res) => {
-  res.redirect('http://13.57.148.57/api/restaurants/' + req.params.id + '/gallery');
+  res.redirect('http://54.177.255.219:3001/api/restaurants/' + req.params.id + '/gallery');
 });
 
 router.get('/:id/overview', (req, res) => {
-  res.redirect('http://184.169.248.150/api/restaurants/' + req.params.id + '/overview');
+  res.redirect('http://54.177.255.219:3002/api/restaurants/' + req.params.id + '/overview');
 });
 
 router.get('/:id/sidebar', (req, res) => {
-  res.redirect('http://54.177.233.239/api/restaurants/' + req.params.id + '/sidebar');
+  res.redirect('http://54.177.255.219:3003/api/restaurants/' + req.params.id + '/sidebar');
 });
 
 router.get('/:id/recommendations', (req, res) => {
-  res.redirect('http://52.89.102.101/api/restaurants/' + req.params.id + '/recommendations');
+  res.redirect('http://54.177.255.219:3004/api/restaurants/' + req.params.id + '/recommendations');
 });
 
 
